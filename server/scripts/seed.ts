08d18f7fe2e0ef3b70e69c9d6e2ed38a4bebf53e
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-08-22 22:29:56 +0530

    setup: seed file (#32)

---
+++ server/scripts/seed.ts
@@ -0,0 +1,16 @@
+import { hash } from 'argon2';
+import { db } from '../src/drizzle';
+import { users } from '../src/auth/schema';
+import { generateIdFromEntropySize } from 'lucia';
+import { hashOptions } from '../src/auth/lucia';
+
+const godUser1 = {
+  id: generateIdFromEntropySize(16),
+  name: 'Nishant Aanjaney Jalan',
+  email: 'nj421@ic.ac.uk',
+  password: await hash('pass#1234', hashOptions),
+  role: 'god',
+  pronouns: 'he/him'
+} as const;
+
+await db.insert(users).values(godUser1);
