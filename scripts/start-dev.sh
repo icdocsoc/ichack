113ffc6c0eb0b78f27b14a213b9bb6a8fc752722
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-12-03 18:47:18 +0000

    fix: docker in dev mode with no network initialisation (#53)

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,8 +1,26 @@
 function cleanup() {
-  docker compose down
+  docker stop postgres
 }
 trap cleanup EXIT
 
-docker compose up -d postgres
+# Check if a container with name postgres exists
+if docker ps -a --format '{{.Names}}' | grep -Eq "^postgres$"; then
+  docker start postgres
+else
+  docker run -d \
+    --name postgres \
+    -e POSTGRES_USER=admin \
+    -e POSTGRES_PASSWORD=rootpasswd \
+    -e POSTGRES_DB=postgres \
+    -p 5432:5432 \
+    -v ichack25_postgres_data:/var/lib/postgresql/data \
+    postgres:16
+fi
+
+while ! docker exec postgres pg_isready -U test > /dev/null; do
+  echo "--- Waiting for Postgres to start ---"
+  sleep 1
+done
+
 bun --bun nuxt prepare
 bun --bun nuxt dev
