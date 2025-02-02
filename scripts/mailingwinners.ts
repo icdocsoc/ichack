ad18d0cfc498a79497cafcf3143cbd248de19057
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2025-02-02 14:33:26 +0000

    fixed script

--- scripts/mailingwinners.ts
+++ scripts/mailingwinners.ts
@@ -21,19 +21,15 @@ interface Output {
   teamPhones: string[];
 }
 
-const path = prompt('File of csv relative to calling directory');
+const path = prompt('File of csv relative to calling directory?');
 if (!path) {
   console.error('path is null');
   process.exit(1);
 }
 
-const content = await Bun.file(path).text();
-
-const records: any[] = parse(content, { bom: true, trim: true, columns: true });
-
-const output: Output[] = [];
-
 const theOperation = async (teamId: number) => {
+  const output: Output[] = [];
+
   const winnerTeamQuery = await db
     .select({
       teamName: teams.teamName,
@@ -42,32 +38,52 @@ const theOperation = async (teamId: number) => {
     })
     .from(teams)
     .where(eq(teams.id, teamId));
+  const winnerMembersQuery = await db
+    .select({
+      teamMember: teamMembers.userId
+    })
+    .from(teams)
+    .innerJoin(teamMembers, eq(teams.id, teamMembers.teamId))
+    .where(eq(teams.id, teamId));
 
   const winnerTeam = winnerTeamQuery[0]!;
+  console.log('winnerTeam', winnerTeam);
+  const winnerMemberIDs = winnerMembersQuery.map(e => e.teamMember);
+  console.log('winnerMemberIDs', winnerMemberIDs);
 
-  const winnerMembers = await db
-    .select({
-      userId: users.id,
-      name: users.name,
-      email: users.email
-    })
-    .from(teamMembers)
-    .innerJoin(users, eq(users.id, teamMembers.userId));
+  for (const memberId of winnerMemberIDs) {
+    const userDetails = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, memberId));
+    const member = userDetails[0]!;
 
-  for (const member of winnerMembers) {
     output.push({
       subject: SUBJECT,
       to: member.email,
       name: member.name,
-      userID: member.userId,
+      userID: member.id,
       teamName: winnerTeam.teamName,
       teamPhones: [winnerTeam.phone ?? '', winnerTeam.phone2 ?? '']
     });
   }
 
+  // for (const member of winnerMembers) {
+  //   output.push({
+  //     subject: SUBJECT,
+  //     to: member.email,
+  //     name: member.name,
+  //     userID: member.userId,
+  //     teamName: winnerTeam.teamName,
+  //     teamPhones: [winnerTeam.phone ?? '', winnerTeam.phone2 ?? '']
+  //   });
+  // }
+
   return output;
 };
 
+const content = await Bun.file(path).text();
+const records: any[] = parse(content, { bom: true, trim: true, columns: true });
 let finalOutput: Output[] = [];
 
 for (const i in records) {
@@ -81,20 +97,14 @@ for (const i in records) {
   let runnerUpId: number;
   try {
     winnerId = parseInt(winner);
-    runnerUpId = parseInt(runnerUp);
-  } catch (e) {
-    console.error('parsing team id went wrong');
-    process.exit(1);
-  }
+    const winnerOutput = await theOperation(winner);
+    finalOutput = [...finalOutput, ...winnerOutput];
 
-  const output: Output[] = [
-    ...(await theOperation(winner)),
-    ...(await theOperation(runnerUp))
-  ];
-  finalOutput = {
-    ...finalOutput,
-    ...output
-  };
+    runnerUpId = parseInt(runnerUp);
+    const runnerUpOutput = await theOperation(runnerUpId);
+    finalOutput = [...finalOutput, ...runnerUpOutput];
+  } catch (e) {}
 }
 
-console.log(finalOutput);
+const file = Bun.file('output.json');
+file.write(JSON.stringify(finalOutput));
